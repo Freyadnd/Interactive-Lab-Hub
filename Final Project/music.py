e8cf1342d8d36e080b43131da20effcb3ef6ee5a
srjsissel <rs923@cornell.edu>
2023-11-30 14:22:36 -0500

    Add volume control for beats

--- Final Project/music.py
+++ Final Project/music.py
@@ -24,11 +24,12 @@ button = digitalio.DigitalIO(seesaw, 24)
 
 beats = ["hi-hat.wav", "Acoustic Sticks 01.wav", "snare-drum.wav", "Acoustic Snare 29.wav"]
 
-def play_beat(index):
+def play_beat(index, volume):
     instance = vlc.Instance()
     p = instance.media_player_new()
     m = instance.media_new(beats[index]) 
     p.set_media(m)
+    vlc.libvlc_audio_set_volume(p, volume)
     p.play()
 
 def run_example():
@@ -36,8 +37,8 @@ def run_example():
     instance = vlc.Instance('--aout=alsa')
     p = instance.media_player_new()
     m = instance.media_new('drum.wav') 
-    instance.vlm_set_loop('drum.wav', True)
-    print(instance.vlm_set_loop('drum.wav', True))
+    instance.vlm_set_loop("drum", True)
+    # print(instance.vlm_set_loop('drum.wav', True))
     p.set_media(m)
     p.play()
     is_playing = True
@@ -45,8 +46,8 @@ def run_example():
     last_position = None
     button_held = False
     encoder = rotaryio.IncrementalEncoder(seesaw)
+    initial_volume = 100
     volume = 100
-    last_pressed = None
 
     while True: 
         if my_button.is_button_pressed() == True:
@@ -66,8 +67,9 @@ def run_example():
             last_position = position
             print("Position: {}".format(position))
             if -20 <= position and position <= 20:
-                vlc.libvlc_audio_set_volume(p, volume + position * 5)
-                print("Volume: {}".format(volume + position * 5))
+                volume = initial_volume + position * 5
+                vlc.libvlc_audio_set_volume(p, volume)
+                print("Volume: {}".format(volume))
 
         if not button.value and not button_held:
             button_held = True
@@ -78,28 +80,28 @@ def run_example():
             print("Button released")
 
         if neokey[0]:
-            play_beat(0)
+            play_beat(0, volume)
             neokey.pixels[0] = 0xFF0000
             time.sleep(0.2)
         else:
             neokey.pixels[0] = 0x0
 
         if neokey[1]:
-            play_beat(1)
+            play_beat(1, volume)
             neokey.pixels[1] = 0xFFFF00
             time.sleep(0.2)
         else:
             neokey.pixels[1] = 0x0
 
         if neokey[2]:
-            play_beat(2)
+            play_beat(2, volume)
             neokey.pixels[2] = 0x00FF00
             time.sleep(0.2)
         else:
             neokey.pixels[2] = 0x0
 
         if neokey[3]:
-            play_beat(3)
+            play_beat(3, volume)
             neokey.pixels[3] = 0x00FFFF
             time.sleep(0.2)
         else:
